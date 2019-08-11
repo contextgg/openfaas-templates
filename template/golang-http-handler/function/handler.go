d6c14e890342a6f7ef4f4222a0bc9d64690ba044
Chris Kolenko <chris@webcanvas.com.au>
2019-08-11 20:35:51 +1000

    Add our own handler

---
+++ template/golang-http-handler/function/handler.go
@@ -0,0 +1,25 @@
+package function
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// NewHandler creates a new http handler
+func NewHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var input []byte
+	
+		if r.Body != nil {
+			defer r.Body.Close()
+	
+			body, _ := ioutil.ReadAll(r.Body)
+	
+			input = body
+		}
+	
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hello world, input was: %s", string(input))))
+	})
+}
