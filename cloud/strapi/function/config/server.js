2ac7f1a50a37fbaaa08ba2e971d7b701a30b2ecd
Chris Kolenko <chris@webcanvas.com.au>
2021-02-14 13:03:07 +1100

    Add Strapi template

---
+++ cloud/strapi/function/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3370216392c1522d9c83ec3e6d38f749'),
+    },
+  },
+});
---
+++ cloud/strapi/function/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8f738dea-783b-4d71-a7f4-11a1557549e9'
+};
